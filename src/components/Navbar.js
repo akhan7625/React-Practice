5a1268fbb7dc6ffbee3d11c4de2bed9de93b1b03
ayeshakhan <akhan7625@gmail.com>
2021-07-27 20:39:13 +0100

    added button function and styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -34,7 +34,7 @@ function Navbar() {
                             </Link>
                         </li>
                         <li className='nav-item'>
-                            <Link to='/signup' className='nav-links-mobile' onClick={closeMobileMenu}>
+                            <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
                                 Sign Up
                             </Link>
                         </li>
