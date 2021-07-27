dcd45c7c168b63e1e96ffe796b0c66184a7b1fff
ayeshakhan <akhan7625@gmail.com>
2021-07-27 20:48:33 +0100

    added button fxn and navbar css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,23 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
     const [click, setClick] = useState(false);
+    const [button, setButton] =useState(true);
 
     const handleClick = () => setClick(!click);
 
     const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+            setButton(false)
+        } else {
+            setButton(true);
+        }
+    };
+
+    window.addEventListener('resize', showButton);
+// show button function determines when button is displayed based on screen size, aka shrinking size or mobile
+
     return (
         <>
             <nav className='navbar'>
@@ -39,6 +52,8 @@ function Navbar() {
                             </Link>
                         </li>
                     </ul>
+                    {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
+                    {/* shortcut, if button is true it'll automatically return whatever is after */}
                 </div>
             </nav>
         </>
